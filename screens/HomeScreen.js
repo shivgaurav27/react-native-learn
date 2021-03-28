bdcd9b2801a9b9c25d246bd1c8d1b663ac4357b1
shivgaurav <shivgaurav27@gmail.com>
2021-03-28 17:11:38 +0530

    rendering flat list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import CustomListItem from "../components/CustomListItem";
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>This is a home page</Text>
-    </View>
-  );
+  return <CustomListItem DATA={DATA} />;
 };
 
 export default HomeScreen;
