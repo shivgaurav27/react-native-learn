60043be4527a2c1419e3c29a845c9141ad70cae3
shivgaurav <shivgaurav27@gmail.com>
2021-03-28 11:20:48 +0530

    shop commit

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2c6bed" },
+  headerTitleStyle: { color: "#fff" },
+  headerTintColor: "#fff",
+};
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app 5!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/LoginScreen.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Image, Input } from "react-native-elements";
+import { KeyboardAvoidingView } from "react-native";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri:
+            "https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/06/shopping-online.jpg",
+        }}
+        style={{ width: 200, height: 200, borderRadius: 10, marginBottom: 20 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          autoFocus
+          type="password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} title="Login" onPress={signIn} />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
